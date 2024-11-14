4ef118d99a9f30aeb93b70300ea960b54812fa2b
Akshay Patil <81678817+Akshay5596@users.noreply.github.com>
2024-11-14 16:55:37 +0530

    Update backend.tf

--- Jump-Server-TF/backend.tf
+++ Jump-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-poc-bucketforstatefile"
+    bucket         = "garba-121 "
     region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jump-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
